dbec0bcebd0583b110f7220dde0298e5019f469b
Cheong <xxlsxx1955@gmail.com>
2023-03-24 07:39:27 +0800

    Include Post model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
+  # TODO: Set the root path
   # root "articles#index"
 end
