464abc122069341e3373b9372180a950dc311887
Cheong <xxlsxx1955@gmail.com>
2023-03-24 07:41:24 +0800

    Include Posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :posts, only: %i[new create index]
 
   # TODO: Set the root path
   # root "articles#index"
