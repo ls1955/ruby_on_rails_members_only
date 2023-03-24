28b6597cb67213a8b30e5e9fdf6ef8c536bb25b7
Cheong <xxlsxx1955@gmail.com>
2023-03-24 08:38:50 +0800

    Fix mistakes
    - Move PostsController's methods from models/post.rb to controllers/posts_controller.rb
    - Include submit button in post#new form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,19 @@
 class PostsController < ApplicationController
+  # before_action :require_login, only: %i[new create]
+  def index
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post.save post_params
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:author, :story)
+  end
 end
