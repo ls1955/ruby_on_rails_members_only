2ace265c295db7da7c27387cfb2bd0875036eb40
Cheong <xxlsxx1955@gmail.com>
2023-03-24 12:25:27 +0800

    Create 1:n relationship between user and post

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :posts, only: %i[new create index]
 
   # TODO: Set the root path
   # root "articles#index"
+  root "posts#new"
 end
